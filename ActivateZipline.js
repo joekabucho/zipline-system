e2944597244de62e66c4ce120a3ef9d18d8ea7cc
joe kabucho <32999243+joekabucho@users.noreply.github.com>
2019-01-16 17:11:07 +0300

    Add files via upload

---
+++ ActivateZipline.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+private var ziplineScript : Zipline;
+
+function Start()
+{
+	ziplineScript = GameObject.Find("First Person Controller").GetComponent(Zipline);
+}
+
+function OnTriggerEnter(Other : Collider)
+{
+     if(Other.gameObject.tag == "Player")
+     {
+		if(Input.GetKeyDown("f"))
+		{
+			ziplineScript.UseZipline();
+		}
+     }
+}
---
+++ Zipline.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var endPos : Transform;
+
+function UseZipline()
+{
+	var time : float = 5;
+    var elapsedTime : float = 0;
+    var startingPos : Vector3 = transform.position;
+    
+    while (elapsedTime < time)
+    {
+    	transform.position = Vector3.Lerp(startingPos, endPos.position, (elapsedTime / time));
+    	elapsedTime += Time.deltaTime;
+    	yield;
+    }
+}
